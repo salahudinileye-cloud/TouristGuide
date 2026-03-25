e0a9ec5c771ef54b326f7fc5fcfac4714947e194
JohnCena1306 <oliverjoostpersson@icloud.com>
2026-03-25 18:04:31 +0100

    Nu virker det lokalt

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS city;
+DROP TABLE IF EXISTS tourist_attraction;
+
 CREATE TABLE IF NOT EXISTS city
 (
     id   INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100)
 );
 
-DROP TABLE IF EXISTS tourist_attraction;
 CREATE TABLE IF NOT EXISTS tourist_attraction
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
